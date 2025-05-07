edb9db5058895d997e9bacd2044d5e4b422270d9
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-07 22:40:21 +0530

    feat: add route for deleting an entry from phonebook

--- phonebook_backend/app.js
+++ phonebook_backend/app.js
@@ -1,5 +1,28 @@
 import express from "express";
-import phonebook from "./db/phonebook.js";
+
+let phonebook = [
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
+];
+
 
 const app = new express();
 
@@ -10,10 +33,17 @@ app.get("/api/persons", (req, res) => {
 app.get("/api/persons/:id", (req, res) => {
     const { id } = req.params;
     const person = phonebook.find((person) => person.id === id);
-    if(!person) {
+    if (!person) {
         return res.status(404).json({});
     }
     res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const { id } = req.params;
+    phonebook = phonebook.filter((person) => person.id !== id);
+
+    res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
