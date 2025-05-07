ffadb9ea6bce4074c396a5773f7da7972c4dc9f5
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-07 22:27:47 +0530

    feat: add route for fetching details for single person

--- phonebook_backend/app.js
+++ phonebook_backend/app.js
@@ -7,6 +7,15 @@ app.get("/api/persons", (req, res) => {
     res.json(phonebook);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+    const { id } = req.params;
+    const person = phonebook.find((person) => person.id === id);
+    if(!person) {
+        return res.status(404).json({});
+    }
+    res.json(person);
+});
+
 app.get("/info", (req, res) => {
     const date = new Date().toString();
     const infoText = `Phonebook has info for ${phonebook.length} people`;
@@ -18,7 +27,7 @@ app.get("/info", (req, res) => {
 });
 
 app.use("/*splat", (req, res) => {
-    res.send("This is not a valid endpoint");
+    res.status(404).send("This is not a valid endpoint");
 });
 
 app.listen(3001, () => console.log("the server is running at port 3001"));
