3e7e0a05b5a6c7480653e6f17ca050798ea2e172
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-05-07 22:18:39 +0530

    feat: set endpoint for general info

--- phonebook_backend/app.js
+++ phonebook_backend/app.js
@@ -7,6 +7,16 @@ app.get("/api/persons", (req, res) => {
     res.json(phonebook);
 });
 
+app.get("/info", (req, res) => {
+    const date = new Date().toString();
+    const infoText = `Phonebook has info for ${phonebook.length} people`;
+
+    res.send(`<div>
+        <p>${infoText}</p>
+        <p>${date}</p>
+    </div>`);
+});
+
 app.use("/*splat", (req, res) => {
     res.send("This is not a valid endpoint");
 });
